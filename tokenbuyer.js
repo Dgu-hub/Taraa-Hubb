eff191cbf7a701975c4623742d11540e6f5ff608
RealzSkyHub <alqhifari.dewantara@gmail.com>
2025-07-25 19:17:06 +0700

    Update tokenbuyer.js

--- tokenbuyer.js
+++ tokenbuyer.js
@@ -1,6 +1,3 @@
-{
-  "tokens": [
-    "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
-    "8101407041:AAFi0Fu3TRS7g4Q4gkKQKEpok4FIgotsL4Y"
-  ]
-}
+const allowedTokens = ['8101407041:AAFi0Fu3TRS7g4Q4gkKQKEpok4FIgotsL4Y']; // contoh token
+
+module.exports = allowedTokens;
